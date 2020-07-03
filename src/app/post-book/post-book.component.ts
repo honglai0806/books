dfd7c64635188e04442da366e0e07bd6e1af8cb4
Hong <16130394@st.hcmuaf.edu.vn>
2020-07-03 13:26:12 +0700

    updateUI

--- src/app/post-book/post-book.component.ts
+++ src/app/post-book/post-book.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-post-book',
   templateUrl: './post-book.component.html',
-  styleUrls: ['./post-book.component.css']
+  styleUrls: ['./post-book.component.scss']
 })
 export class PostBookComponent implements OnInit {
 
